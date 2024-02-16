da77496f85494de2afa423e94f7f14ae442d3573
Sabina <109144680+Srasulova@users.noreply.github.com>
2024-02-16 13:11:45 -0500

    finished wordfinder

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,16 @@
 """Word Finder: finds random words from a dictionary."""
+import random
 
 
 class WordFinder:
-    ...
+    def __init__(self, path):
+        self.file = open(path)
+        self.word_lst = self.file.read().split()
+        self.file.close()
+        print(f"{len(self.word_lst)} words read")
+
+    def random(self):
+        return random.choice(self.word_lst)
+
+wf = WordFinder("words.txt")
+print(wf.random())
